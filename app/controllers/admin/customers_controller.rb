c7c9df78a71890fd70ff9a19f895426cd3ddc44a
west03 <zxi6557@gmail.com>
2021-10-10 01:49:54 +0000

    admin_customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,21 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+   private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_active)
+  end
+
 end
