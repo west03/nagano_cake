581ea3d7c48c83eaf44b6079ae10695e2484d341
west03 <zxi6557@gmail.com>
2021-10-16 13:02:12 +0000

    admin_order

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,3 +1,9 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
+  
+  
+   # 違うテーブル
+  def  order_detail_params
+    params.req
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,10 +4,22 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    
   end
 
   def update
+    @order = Order.find(params[:id])
+    if @order.update(params[:id])
+      redirect_to  admin_order_path(order.id)
+    else
+      render :show
+    end
   end
 
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 
 end
